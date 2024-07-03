1292ca33dc18af017a3e809fb917df38189a38ee
CrisGarciF <garciamesacris543@gmail.com>
2024-07-03 15:10:15 -0500

    Desafios Alura

---
+++ E-COMMERCE/controllers/search.js
@@ -0,0 +1,22 @@
+const container = document.getElementById('search__container');
+const search = document.getElementById('search__items');
+const action = document.getElementById('search__action');
+
+let products = [];
+
+setInterval(() => {
+    products = Array.from(document.querySelectorAll('[data-product]'));
+}, 1000 / 5);
+
+const update = () => {
+    const value = search.value.toLowerCase();
+
+    for (const product of products) {
+        const name = (product.getAttribute('data-product') || '').toLowerCase().trim();
+
+        product.style.display = !value || !name || name.includes(value) ? 'flex' : 'none';
+    }
+};
+
+search.addEventListener('input', update);
+search.addEventListener('change', update);
